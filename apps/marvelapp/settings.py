bee56f941d760d735b00ef4c5d566ac47aa66bfb
Juan Collin <juan.collin@poatek.com>
2021-10-09 20:20:36 -0300

    Adicionados os htmls da página home e dos detalhes do homem aranha

--- apps/marvelapp/settings.py
+++ apps/marvelapp/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'marvelapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(os.path.dirname(__file__) ,'../templates').replace('\\','/')],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- apps/marvelapp/urls.py
+++ apps/marvelapp/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls', namespace='core')),
+    path('', views.home, name='home'),
+    path('core/', include('core.urls', namespace='core')),
+    path('spiderman/', views.spiderman, name='spiderman'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
