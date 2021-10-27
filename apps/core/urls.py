b3714d877d629ebd2f7008105c9a5afc8109e4df
Juan Collin <juan.collin@poatek.com>
2021-10-27 20:39:57 -0300

    Pequenos ajustes

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -5,4 +5,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('characters/', views.characters, name='characters'),
+    path('spiderman/', views.spiderman, name='spiderman'),
+    path('movies/', views.movies, name='movies'),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -5,3 +5,18 @@ def home(request):
     template_name ='core/home.html'
     context = {}
     return render(request, template_name, context)
+
+def characters(request):
+    template_name ='marvelapp/characters/characters.html'
+    context = {}
+    return render(request, template_name, context)
+
+def spiderman(request):
+    template_name ='marvelapp/characters/spiderman.html'
+    context = {}
+    return render(request, template_name, context)
+
+def movies(request):
+    template_name ='marvelapp/movies/movies.html'
+    context = {}
+    return render(request, template_name, context)
